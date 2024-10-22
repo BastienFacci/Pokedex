11ac2a1660d115a8960bb6ff86d7a5b206940480
Faccinetto <bastien.faccinetto@gmail.com>
2024-10-22 15:21:58 +0200

    Création Pokemon card

---
+++ src/components/PokemonCard.tsx
@@ -0,0 +1,5 @@
+function PokemonCard() {
+    return <figure><img src="https://static0.gamerantimages.com/wordpress/wp-content/uploads/2019/11/pokemon-charmander.jpg?q=50&fit=crop&w=1140&h=&dpr=1.5" alt="Le plus cute des pokemon"/><figcaption>Salamèche</figcaption></figure>;
+  }
+  
+  export default PokemonCard;
