964e6aa7646f801f75f3527a524c8ae176a879ff
Faccinetto <bastien.faccinetto@gmail.com>
2024-10-23 17:26:32 +0200

    Quête 4 done

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,26 +1,20 @@
-function PokemonCard() {
-    const pokemon = pokemonList[1];
-    //En changeant l'index de pokemonlist, on peut afficher burbasaur
-    return <figure>
-        {pokemon.imgSrc ? (
-        <img src = {pokemon.imgSrc} alt = {pokemon.name} />
-        ) : (
-            <p>???</p>
-        )}
-
-     <figcaption>{pokemon.name}</figcaption>
-        </figure>;
-  }
+interface PokemonProps {
+    imgSrc: string;
+    name: string;
   
-  export default PokemonCard;
+  }
 
-  const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
+  function PokemonCard({pokemon}) {
+    return (
+      <figure>
+        {pokemon.imgSrc ? ( 
+          <img src={pokemon.imgSrc} alt={pokemon.name} />
+        ) : (
+        <p>???</p>
+        )}
+        <figcaption>{pokemon.name}</figcaption>
+        </figure>
+    );
+  }
+
+  export default PokemonCard;
